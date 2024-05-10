1ca38115c6a98ea52b6f28196b67efac9762eb14
Leonardo1205 <leonardoreitz0@gmail.com>
2024-05-10 17:19:56 -0300

    adding Button component

---
+++ src/components/Button/Index.jsx
@@ -0,0 +1,10 @@
+import { Container  } from "./Styles";
+
+export function Button() {
+
+    return (
+        <Container type="button">
+        Clique aqui
+        </Container>
+    )
+}
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,14 @@
 import { Container } from "./styles"
 
+import { Button } from "../../components/Button/Index"
+
 export function Details() {
   return(
     <Container>
-    <h1>Hello World!</h1>
-    <span>Leonardo Reitz</span>
+      <h1>Hello World!</h1>
+      <span>Leonardo Reitz</span>
+
+      <Button />
     </Container>
   )
 }
