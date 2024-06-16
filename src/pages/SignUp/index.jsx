d4d57cf0d7cec5a06da2bd820c5096f6b0666050
Leonardo1205 <leonardoreitz0@gmail.com>
2024-06-15 21:22:45 -0300

    SignIn and SignUp page

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,16 +1,24 @@
-import { FiMail, FiLock } from 'react-icons/fi'
+import { FiMail, FiLock, FiUser } from 'react-icons/fi'
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button/Index";
 import { Container, Form, Background } from "./styles";
 
-export function SignIn() {
+export function SignUp() {
     return(
         <Container>
+            
+            <Background/>
             <Form>
                 <h1>Rocket Notes</h1>
                 <p>Aplicação para salvar e gerenciar seus links úteis.</p>
 
-                <h2>Faça seu login</h2>
+                <h2>Crie sua conta</h2>
+
+                <Input
+                    placeHolder = "Nome"
+                    type= "text"
+                    icon={FiUser}
+                />
 
                 <Input
                     placeHolder = "E-mail"
@@ -24,14 +32,13 @@ export function SignIn() {
                     icon={FiLock}
                 />
 
-                <Button title= "Entrar"/>
+                <Button title= "Cadastrar"/>
                     
                 <a href="#">
-                    Criar conta
+                    Voltar para o login
                 </a>
             </Form>
 
-            <Background/>
         </Container>
     );
 }
