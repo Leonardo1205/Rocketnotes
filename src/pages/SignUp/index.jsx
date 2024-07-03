2f4c0d9dfb07d0a2f2d7bcd95222a16133893a6a
Leonardo1205 <leonardoreitz0@gmail.com>
2024-07-03 09:36:30 -0300

    Navigation

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,4 +1,5 @@
 import { FiMail, FiLock, FiUser } from 'react-icons/fi'
+import { Link } from "react-router-dom"
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button/Index";
 import { Container, Form, Background } from "./styles";
@@ -34,9 +35,9 @@ export function SignUp() {
 
                 <Button title= "Cadastrar"/>
                     
-                <a href="#">
+                <Link to="/">
                     Voltar para o login
-                </a>
+                </Link>
             </Form>
 
         </Container>
