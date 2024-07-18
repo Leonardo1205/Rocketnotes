9bad0f5d8179eea1baf6e37fa9141ede4f6fbe5a
Leonardo1205 <leonardoreitz0@gmail.com>
2024-07-18 20:00:59 -0300

    creating context

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from "styled-components"
+import { MyContext } from './myContext';
 import GlobalStyles from "./styles/global"
 import  theme  from "./styles/theme"
 import { Routes } from './routes' 
@@ -11,7 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Routes/>
+      <MyContext.Provider value={{ name: 'LeoReitz',   email: 'leo@email.com' }}>
+        <Routes />
+      </MyContext.Provider>
     </ThemeProvider>
   </React.StrictMode>
 )
