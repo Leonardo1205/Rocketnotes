16455e1ad645bcbe6c933935284542e778ce952d
Leonardo1205 <leonardoreitz0@gmail.com>
2024-07-24 19:00:42 -0300

    using localStorage to store datain the browser

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,6 +1,7 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 import { api } from "../services/api";
+import { json } from "react-router-dom";
 
 export const AuthContext = createContext({});
 
@@ -12,6 +13,9 @@ function AuthProvider({ children }) {
         try {
             const response = await api.post("/sessions", { email, password });
             const { user, token } = response.data;
+            
+            localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
+            localStorage.setItem("@rocketnotes:token", token);
 
             api.defaults.headers.authorization = `Bearer ${token}`;
             setData({ user, token });
@@ -25,6 +29,21 @@ function AuthProvider({ children }) {
         }
         
     }
+
+    useEffect(() => {
+        const token = localStorage.getItem("@rocketnotes:token");
+        const user = localStorage.getItem("@rocketnotes:user");
+
+        if( token && user) {
+            api.defaults.headers.authorization = `Bearer ${token}`;
+
+            setData({
+                token,
+                user: JSON.parse(user)
+            });
+        }
+
+    }, []);
 
     return (
         <AuthContext.Provider value={{ signIn, user: data.user }}>
